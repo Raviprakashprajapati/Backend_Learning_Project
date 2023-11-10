16b61c0ad5d0dbcbf10d8d2684fb8d65ffbc23cf
Ravi prakash prajapati <raviprakashprajapati445@gmail.com>
2023-11-10 18:45:11 +0530

    custom errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // require("dotenv").config()
+import { app } from "./app.js";
 import connectDB from "./db/db.js";
 import dotenv from "dotenv"
 
@@ -8,7 +9,15 @@ dotenv.config({
 
 
 connectDB()
+.then((value)=>{
 
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is listening on ${process.env.PORT}`)
+    })
+
+}).catch((err)=>{
+    console.log("ERROR IN DB ", err)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+const asyncHandler = (reqhandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(reqhandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+
+
+export {asyncHandler}
+
+
+// const asyncHandler=()=>{}
+// const asyncHandler=(fn)=>()=>{}
+// const asyncHandler=(fn)=>async()=>{}
+
+// const asyncHandler=(fn)=>async(req,res,next)=>{
+
+//     try {
+
+//         await fn(req,res,next)
+        
+//     } catch (error) {
+//         res.status().json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+
+// }
